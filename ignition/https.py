b71dbd1060296d4bc6d5b808cdd36b06c2da3023
Péter Vámos <pvamos@gmail.com>
2022-06-07 21:11:27 +0000

    https ignition

---
+++ ignition/https.py
@@ -0,0 +1,14 @@
+# HTTPS server
+#   generate self signed certificate with:
+#   openssl req -new -x509 -keyout private.pem -out certificate.pem -days 365 -nodes 
+#
+
+from http.server import HTTPServer,SimpleHTTPRequestHandler
+import ssl
+
+httpsrv = HTTPServer(('', 4443), SimpleHTTPRequestHandler)
+sslctx = ssl.SSLContext(protocol = ssl.PROTOCOL_TLS_SERVER, check_hostname = False)
+sslctx.load_cert_chain(certfile='certificate.pem', keyfile="private.pem")
+httpsrv.socket = sslctx.wrap_socket(httpsrv.socket, server_side=True)
+httpsrv.serve_forever()
+
